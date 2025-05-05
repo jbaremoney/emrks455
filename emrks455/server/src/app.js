558df1ff12fa279f3119f1821f4266180d1bb87d
OTschmoe <owenreilly129@gmail.com>
2025-05-05 10:27:18 -0500

    Deleting comment
    Deleted comment

--- emrks455/server/src/app.js
+++ emrks455/server/src/app.js
@@ -1,26 +1,3 @@
-// const express = require('express');
-// const cors = require('cors');
-// // const morgan = require('morgan'); // Optionally for logging
-// // const bodyParser = require('body-parser'); // Or just use express.json()
-
-// const patientRoutes = require('../routes/patientRoutes');
-
-// const app = express();
-
-// // Middlewares
-// app.use(cors());
-// app.use(express.json()); // parse JSON bodies
-// // app.use(morgan('dev')); // if you want simple request logging
-
-// // Routes
-// app.use('/api/users', patientRoutes);
-
-// // (Optional) Global error handler or 404 handler
-// app.use((req, res, next) => {
-//   res.status(404).json({ message: 'Route not found' });
-// });
-
-// module.exports = app;
 const express = require('express');
 const cors = require('cors');
 
